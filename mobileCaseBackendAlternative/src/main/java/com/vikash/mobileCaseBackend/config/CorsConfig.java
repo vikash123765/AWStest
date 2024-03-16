e2e4d0d4a8f989b6f68a9f99adde5acdef51fd2e
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-03-16 11:58:34 -0800

    corsconfig chnages

--- mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
+++ mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {@Override
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
             .allowedHeaders("*"); // You can specify specific headers if needed
 }
 }
