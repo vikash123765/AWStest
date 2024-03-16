6e3dd08fa72521170924576deffbf252847c0b97
vikash123765 <92863236+vikash123765@users.noreply.github.com>
2024-03-16 11:26:48 -0800

    trying to conect the back end and front end configuratioon adde in corsconifig

--- mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
+++ mobileCaseBackendAlternative/src/main/java/com/vikash/mobileCaseBackend/config/CorsConfig.java
@@ -1,18 +1,14 @@
-
 package com.vikash.mobileCaseBackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*"); // You can specify specific headers if needed
-    }
+public class CorsConfig implements WebMvcConfigurer {@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*"); // You can specify specific headers if needed
 }
-
+}
